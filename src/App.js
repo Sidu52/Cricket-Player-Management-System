632205ffea36130287ff4686e87efbfff002ff80
sidu52 <hitechsidu992@gmail.com>
2024-02-27 17:59:41 +0530

    Cricket Player Management System.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import playersData from './data/playerdata'
 
-function App() {
+const App = () => {
+  const [sort, setSort] = useState();
+  const [name, setName] = useState('');
+  const [minAverage, setMinAverage] = useState();
+  const [maxAverage, setMaxAverage] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+  const playersPerPage = 5; //Number of Player on Per Page
+  // Pagination for Page Change
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  // Players Sort In Ascending and Descending
+  const sortedPlayers = playersData.slice().sort((a, b) => {
+    if (sort === 'ascending') {
+      return a.battingAverage - b.battingAverage;
+    } else if (sort === 'descending') {
+      return b.battingAverage - a.battingAverage;
+    }
+    return 0;
+  });
+
+  // Players Filter By Name, Minimum Average and Maximum Average
+  const filteredPlayers = sortedPlayers.filter(playerData =>
+    playerData.name.toLowerCase().includes(name.toLowerCase()) && (!minAverage || playerData.battingAverage >= parseFloat(minAverage)) && (!maxAverage || playerData.battingAverage <= parseFloat(maxAverage))
+  );
+
+  // Pagination
+  const indexOfLastPlayer = currentPage * playersPerPage;
+  const indexOfFirstPlayer = indexOfLastPlayer - playersPerPage;
+  const currentPlayer = filteredPlayers.slice(indexOfFirstPlayer, indexOfLastPlayer);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-5'>
+      <h2 className='w-full text-center text-4xl font-bold'>Cricket Player Management System</h2>
+
+      <div className='w-full flex flex-wrap items-center justify-evenly mt-5'>
+        <div className='flex flex-col relative'>
+          <label className='text-gray-800 font-medium'>Name</label>
+          <input className='border-b-2 border-black outline-none px-2' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className='flex flex-col relative'>
+          <label className='text-gray-800 font-medium'>Min-Average</label>
+          <input className='border-b-2 border-black outline-none px-2' type="number" value={minAverage} onChange={(e) => setMinAverage(e.target.value)} />
+        </div>
+        <div className='flex flex-col relative'>
+          <label className='text-gray-800 font-medium'>Max-Average</label>
+          <input className='border-b-2 border-black outline-none px-2' type="number" value={maxAverage} onChange={(e) => setMaxAverage(e.target.value)} />
+        </div>
+      </div>
+      <table className="w-full mt-5">
+        <thead>
+          <tr className="bg-gray-300">
+            <th className="py-2 px-4">Name</th>
+            <th className="py-2 px-4">Country</th>
+            <th className="py-2 px-4">Batting-Averages
+              <button className="bg-black text-white rounded-full p-2 text-sm ml-2" onClick={() => setSort('ascending')}>Asc</button>
+              <button className="bg-black text-white rounded-full p-2 text-sm ml-2" onClick={() => setSort('descending')}>Des</button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentPlayer.map(playerData => (
+            <tr key={playerData.id} className="border-t text-center">
+              <td className="py-2 px-4">{playerData.name}</td>
+              <td className="py-2 px-4">{playerData.country}</td>
+              <td className="py-2 px-4">{playerData.battingAverage}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-5">
+        <ul className="flex items-center justify-center gap-2">
+          {Array(Math.ceil(filteredPlayers.length / playersPerPage))
+            .fill()
+            .map((_, i) => (
+              <li key={i} className="mr-2">
+                <button onClick={() => paginate(i + 1)} className="rounded-full px-2 py-1 text-sm bg-gray-500 border-2 text-white hover:text-gray-500 hover:bg-transparent">{i + 1}</button>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/data/playerdata.js
@@ -0,0 +1,24 @@
+const playersData = [
+    { id: 1, name: 'Virat Kohli', country: 'India', battingAverage: 59.58 },
+    { id: 2, name: 'Steve Smith', country: 'Australia', battingAverage: 62.84 },
+    { id: 3, name: 'Kane Williamson', country: 'New Zealand', battingAverage: 53.38 },
+    { id: 4, name: 'Joe Root', country: 'England', battingAverage: 52.8 },
+    { id: 5, name: 'David Warner', country: 'Australia', battingAverage: 48.94 },
+    { id: 6, name: 'Babar Azam', country: 'Pakistan', battingAverage: 45.12 },
+    { id: 7, name: 'Rohit Sharma', country: 'India', battingAverage: 45.05 },
+    { id: 8, name: 'Quinton de Kock', country: 'South Africa', battingAverage: 45.0 },
+    { id: 9, name: 'Kumar Sangakkara', country: 'Sri Lanka', battingAverage: 57.4 },
+    { id: 10, name: 'AB de Villiers', country: 'South Africa', battingAverage: 50.66 },
+    { id: 11, name: 'Hashim Amla', country: 'South Africa', battingAverage: 49.46 },
+    { id: 12, name: 'Shane Watson', country: 'Australia', battingAverage: 35.19 },
+    { id: 13, name: 'Chris Gayle', country: 'West Indies', battingAverage: 38.2 },
+    { id: 14, name: 'Sachin Tendulkar', country: 'India', battingAverage: 53.78 },
+    { id: 15, name: 'Ricky Ponting', country: 'Australia', battingAverage: 51.85 },
+    { id: 16, name: 'Jacques Kallis', country: 'South Africa', battingAverage: 55.37 },
+    { id: 17, name: 'Brian Lara', country: 'West Indies', battingAverage: 52.88 },
+    { id: 18, name: 'MS Dhoni', country: 'India', battingAverage: 38.09 },
+    { id: 19, name: 'Younis Khan', country: 'Pakistan', battingAverage: 52.05 },
+    { id: 20, name: 'Hardik Pandey', country: 'India', battingAverage: 39.18 }
+];
+
+export default playersData;
